d2600088bbac457c6b5faca45af48eeb1872b36d
Elif Beyza <saglame99@gmail.com>
2023-09-07 12:07:52 +0300

    changed location

---
+++ ValorantApp/Modules/Main/View/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  ValorantApp
+//
+//  Created by ELİF BEYZA SAĞLAM on 7.09.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
