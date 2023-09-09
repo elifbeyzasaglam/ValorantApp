dfd65ebde266a505c5bc9ab51372583210c20711
Elif Beyza <saglame99@gmail.com>
2023-09-09 16:23:24 +0300

    created LogoCell.

---
+++ ValorantApp/Modules/Main/Cells/Logo/LogoCell.swift
@@ -0,0 +1,13 @@
+//
+//  LogoCell.swift
+//  ValorantApp
+//
+//  Created by ELİF BEYZA SAĞLAM on 9.09.2023.
+//
+
+import UIKit
+
+final class LogoCell: UITableViewCell {
+    @IBOutlet private weak var logoImageView: UIImageView!
+    
+}
