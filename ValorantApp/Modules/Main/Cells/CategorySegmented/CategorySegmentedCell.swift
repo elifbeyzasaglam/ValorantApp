d870cf898e3028267e51cdfe2a2b95751d3f31ac
Elif Beyza <saglame99@gmail.com>
2023-09-09 16:23:50 +0300

    created CAtegorySegmentedCell.

---
+++ ValorantApp/Modules/Main/Cells/CategorySegmented/CategorySegmentedCell.swift
@@ -0,0 +1,23 @@
+//
+//  CategorySegmentedCell.swift
+//  ValorantApp
+//
+//  Created by ELİF BEYZA SAĞLAM on 9.09.2023.
+//
+
+import UIKit
+
+class CategorySegmentedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
